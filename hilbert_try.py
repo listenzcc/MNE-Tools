8fe907b092f88d4058ba38b2c39a3f66f59b1281
zcc <listenzcc@mail.bnu.edu.cn>
2018-10-09 18:18:14 +0800

    events must be read from the raw object

--- hilbert_try.py
+++ hilbert_try.py
@@ -3,6 +3,18 @@
 import mne
 import numpy as np
 import matplotlib.pyplot as plt
+from scipy import fftpack
+
+
+def get_envlop(x):
+    y = fftpack.hilbert(x)
+    return np.sqrt(x**2 + y**2)
+
+
+def get_hilbert(data, picks):
+    for j in picks:
+        data[j] = get_envlop(data[j])
+    return data
 
 
 # File name
@@ -13,30 +25,60 @@ tmin, tmax = -0.25, 1.25
 freq_l, freq_h = 1, 15
 
 # Load raw data
-raw = mne.io.read_raw_fif(raw_fname, preload=True)
-
-# Filter raw data
-raw.filter(freq_l, freq_h, fir_design='firwin')
-
-# Setting sensors to pick
-picks = mne.pick_types(raw.info, meg=True, eeg=False, stim=False, eog=False,
+raw_old = mne.io.read_raw_fif(raw_fname, preload=True)
+raw_old.filter(freq_l, freq_h, fir_design='firwin')
+picks = mne.pick_types(raw_old.info,
+                       meg=True, stim=False,
+                       eeg=False, eog=False,
                        exclude='bads')
 
-# Prepare issues for 'EpochsArray'
-info = raw.info
-events = mne.find_events(raw)
+raw = mne.io.RawArray(raw_old.get_data(), raw_old.info)
+raw_custom = mne.io.RawArray(get_hilbert(raw.get_data(), picks), raw.info)
+
+# event_ids
 event_ids = dict(ort1=2, ort2=6, ort3=9, ort4=14, ort5=17, ort6=33)
+
+# raw issues
+info = raw.info
 reject = dict(grad=4000e-13)
 
-# Pick data
-data = raw.get_data()[picks]
-idx = raw.ch_names.index('MEG1941')
-print('idx is %d' % idx)
-data_pick = data[idx]
-timeline = data_pick*0
-timeline[events[0:-2, 0]] = 1.1*max(data_pick)
-plt.plot(data_pick)
-plt.plot(timeline)
-plt.plot(-timeline)
-plt.title(raw.ch_names[idx])
+
+def plot_raw(raw2show, picks=picks):
+    events = mne.find_events(raw2show)
+    epochs_raw = mne.Epochs(raw2show,
+                            events=events, event_id=event_ids,
+                            tmin=tmin, tmax=tmax,
+                            picks=picks, baseline=(tmin, 0),
+                            reject=reject, preload=True)
+    evoked_raw = epochs_raw.average()
+    evoked_raw.plot(time_unit='s', show=False)
+    '''
+    plt.figure()
+    idx = 1
+    for e in event_ids.values():
+        epochs_tmp = mne.Epochs(raw2show,
+                                events=events, event_id=e,
+                                tmin=tmin, tmax=tmax,
+                                picks=picks, baseline=(tmin, 0),
+                                reject=reject, preload=True)
+        evoked_tmp = epochs_tmp.average()
+        plt.subplot(3, 2, idx)
+        idx += 1
+        plt.plot(evoked_tmp.data.transpose())
+        plt.title(e)
+    '''
+
+
+plot_raw(raw_old)
+plot_raw(raw)
+plot_raw(raw_custom)
+
+'''
+plt.figure()
+idx = 255
+plt.plot(raw.get_data()[idx])
+plt.plot(raw_custom.get_data()[idx])
+'''
+
+
 plt.show()
