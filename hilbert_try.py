7fbb62ccc746f87977807d27be76e2ce071fc5f9
zcc <listenzcc@mail.bnu.edu.cn>
2018-10-10 14:30:49 +0800

    correctly restore processed signals into raw object

--- hilbert_try.py
+++ hilbert_try.py
@@ -17,22 +17,25 @@ def get_hilbert(data, picks):
     return data
 
 
-# File name
-raw_fname = 'D:\\BeidaShuju\\rawdata\\QYJ\\MultiTraining_1_raw_tsss.fif'
-
 # Settings
 tmin, tmax = -0.25, 1.25
 freq_l, freq_h = 1, 15
 
 # Load raw data
-raw_old = mne.io.read_raw_fif(raw_fname, preload=True)
-raw_old.filter(freq_l, freq_h, fir_design='firwin')
-picks = mne.pick_types(raw_old.info,
+raw_fname = 'D:\\BeidaShuju\\rawdata\\QYJ\\MultiTraining_1_raw_tsss.fif'
+raw = mne.io.read_raw_fif(raw_fname, preload=True)
+raw = mne.io.RawArray(raw.get_data(), raw.info)
+
+# Picks
+picks = mne.pick_types(raw.info,
                        meg=True, stim=False,
                        eeg=False, eog=False,
                        exclude='bads')
 
-raw = mne.io.RawArray(raw_old.get_data(), raw_old.info)
+# Filter data
+raw.filter(freq_l, freq_h, fir_design='firwin')
+
+# Custom raw
 raw_custom = mne.io.RawArray(get_hilbert(raw.get_data(), picks), raw.info)
 
 # event_ids
@@ -43,42 +46,38 @@ info = raw.info
 reject = dict(grad=4000e-13)
 
 
-def plot_raw(raw2show, picks=picks):
-    events = mne.find_events(raw2show)
-    epochs_raw = mne.Epochs(raw2show,
-                            events=events, event_id=event_ids,
-                            tmin=tmin, tmax=tmax,
-                            picks=picks, baseline=(tmin, 0),
-                            reject=reject, preload=True)
-    evoked_raw = epochs_raw.average()
-    evoked_raw.plot(time_unit='s', show=False)
-    '''
-    plt.figure()
-    idx = 1
-    for e in event_ids.values():
-        epochs_tmp = mne.Epochs(raw2show,
-                                events=events, event_id=e,
-                                tmin=tmin, tmax=tmax,
-                                picks=picks, baseline=(tmin, 0),
-                                reject=reject, preload=True)
-        evoked_tmp = epochs_tmp.average()
-        plt.subplot(3, 2, idx)
-        idx += 1
-        plt.plot(evoked_tmp.data.transpose())
-        plt.title(e)
-    '''
+def get_epochs(raw_object, event_id, picks=picks,
+               tmin=tmin, tmax=tmax,
+               baseline=(tmin, 0),
+               reject=reject, preload=True):
+    events = mne.find_events(raw_object)
+    epochs = mne.Epochs(raw_object,
+                        events=events, event_id=event_id,
+                        tmin=tmin, tmax=tmax,
+                        picks=picks, baseline=(tmin, 0),
+                        reject=reject, preload=True)
+    return epochs
 
 
-plot_raw(raw_old)
-plot_raw(raw)
-plot_raw(raw_custom)
+def plot_evoked(epochs):
+    data = epochs.get_data()
+    data_mean = np.mean(data, axis=0)
+    plt.plot(data_mean.transpose())
+    plt.title(str(epochs.event_id))
 
-'''
+
+# get_epochs(raw, event_ids).average().plot(time_unit='s', show=False)
+# get_epochs(raw_custom, event_ids).average().plot(time_unit='s', show=False)
+
+# Plot epochs
 plt.figure()
-idx = 255
-plt.plot(raw.get_data()[idx])
-plt.plot(raw_custom.get_data()[idx])
-'''
+plot_evoked(get_epochs(raw_custom, event_ids))
 
+plt.figure()
+idx = 1
+for e in event_ids.values():
+    plt.subplot(3, 2, idx)
+    idx += 1
+    plot_evoked(get_epochs(raw_custom, e))
 
 plt.show()
